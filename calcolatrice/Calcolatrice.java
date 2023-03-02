4887a6b8d17c83252253125e46e55fdc37fbc34f
Diego <74570312+spyrash@users.noreply.github.com>
2023-03-02 16:38:20 +0100

    aggiunti esercizi svolti rettangolo e calcolatrice(senza main)

---
+++ calcolatrice/Calcolatrice.java
@@ -0,0 +1,26 @@
+
+public class Calcolatrice {
+
+	int a;
+	int b;
+
+	public void somma() {
+		int s = a + b;
+		System.out.println("la somma è:" + s);
+	}
+
+	public void sottrazione() {
+		int s = a - b;
+		System.out.println("la sottrazione è:" + s);
+	}
+
+	public void divisione() {
+		int s = a / b;
+		System.out.println("la divisione è:" + s);
+	}
+
+	public void moltiplicazione() {
+		int s = a + b;
+		System.out.println("la moltiplicazione è:" + s);
+	}
+}
---
+++ rettangolo/Rettangolo.java
@@ -0,0 +1,16 @@
+
+public class Rettangolo {
+
+	int base;
+	int altezza;
+
+	public void CalcolaArea() {
+		int area = base * altezza;
+		System.out.println("l'area è:" + area);
+	}
+
+	public void calcolaPerimetro() {
+		int perimetro = 2 * base + 2 * altezza;
+		System.out.println("il perimentro è:" + perimetro);
+	}
+}
